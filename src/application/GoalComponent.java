a9c68d947f9773a35e13050a7f18a1f29fc830eb
KatieBurgess <katherine.burgess@ucalgary.ca>
2022-11-22 15:05:23 -0700

    Check if duration time is longer than 12 hours

--- src/application/GoalComponent.java
+++ src/application/GoalComponent.java
@@ -4,7 +4,7 @@ public class GoalComponent {
 	
 	// Instance Variables
 	private double targetWeight;
-	private long duration;
+	private long duration = 720;
 	private double lowerBodyPR;
 	private double upperBodyPR;
 	
@@ -17,9 +17,21 @@ public class GoalComponent {
 			lowerBodyPR = Double.parseDouble(lowerBody);
 			upperBodyPR = Double.parseDouble(upperBody);
 			
+			// Duration of workout cannot be longer than 12 hours 
+			if (this.duration < 0 || this.duration > durationInHours()) {
+				throw new InvalidEntryException(String.format("Duration of workout can only be between 0 and %d ", durationInHours()));
+				
+			}
+			
 		} catch (NumberFormatException e) {
 			throw new InvalidEntryException(String.format("Invalid Goal Entry. Make sure to enter a number."));
 		}
+	}
+
+
+	private long durationInHours() {
+		long hours = duration / 60;
+		return hours;
 	}
 	
 	
